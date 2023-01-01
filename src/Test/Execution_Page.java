c9bb54925b53fac13b1a27dae19b7f919bec0b9a
Ajit77670 <Ajit77670@gmail.com>
2023-01-01 21:34:53 +0530

    commit to the Local Repo

---
+++ src/Test/Execution_Page.java
@@ -0,0 +1,11 @@
+
+package Test;
+
+public class Execution_Page {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("This is execution page");
+    }
+    
+}
